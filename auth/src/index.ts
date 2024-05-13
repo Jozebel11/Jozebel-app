f8e32d3529070f33ae45e4651256bf14a7660295
jozebel11 <jjhardwicke@hotmail.co.uk>
2024-05-13 10:28:34 +0100

    husky test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('*', (req, res) => {
-    res.send({ hello: 'hell' });
+    res.send({ hello: 'hel' });
 });
 
 app.listen(3000, () => {
