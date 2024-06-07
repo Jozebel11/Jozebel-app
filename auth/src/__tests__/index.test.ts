62854ebd629daa327ce8cb6b0ff09e1c93e8836c
jozebel11 <jjhardwicke@hotmail.co.uk>
2024-06-07 21:26:04 +0100

    Commit changes

--- auth/src/__tests__/index.test.ts
+++ auth/src/__tests__/index.test.ts
@@ -1,3 +1,6 @@
 import request from 'supertest';
+import { app } from '../app';
 
-request.agent();
+it('Responds with a status of 200', async () => {
+    await request(app).get('/').expect(200);
+});
--- auth/src/app.ts
+++ auth/src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('*', (req, res) => {
-    res.send({ hello: 'hello world' });
+    res.status(200).send({ hello: 'hello world' });
 });
 
 export { app };
