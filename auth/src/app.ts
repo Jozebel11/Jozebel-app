01338f1abccfa321570def930b84d43253e77034
jozebel11 <jjhardwicke@hotmail.co.uk>
2024-06-24 00:29:41 +0100

    Add signup routes

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { json } from 'body-parser';
+import { signupRouter } from './routes';
 
 const app = express();
 
-app.get('*', (req, res) => {
-    res.status(200).send({});
-});
+app.use(json());
+
+app.use(signupRouter);
 
 export { app };
